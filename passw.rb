52c1edba2bb4846b3455fc5fd1058cabae4450a1
jessor <daniel@enthusiasm.cc>
2011-04-10 15:22:34 +0200

    first working version

--- passw.rb
+++ passw.rb
@@ -9,7 +9,23 @@ configure do
 end
 
 helpers do
-  # ...
+  def simple
+    [('a'..'z'),('0'..'9')].map{|i| i.to_a}.flatten
+  end
+
+  def special
+    [('a'..'z'),('0'..'9'),'!', '§', '$', '%', '&', '/', '(', ')', '=', '?', '²', '³', '{', '[', ']', '}', '~', '#', '-', '_' ].map{|i| i.to_a}.flatten
+  end
+
+  def generate_password(chars)
+    (0..9).map{ chars[rand(chars.length)] }.join
+  end
+
+  def generate_passwords(type)
+    (0...10).map do |i|
+      generate_password(type)
+    end
+  end
 end
 
 get '/stylesheet.css' do
@@ -17,5 +33,6 @@ get '/stylesheet.css' do
 end
 
 get '/' do
+  @passwords = Array.new
   haml :show
 end
